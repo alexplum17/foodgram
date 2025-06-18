cae7947787627ef1e885ccc9c855bc519404dd57
Alex Perov <aleksandrperov97@yandex.ru>
2025-06-18 22:22:22 +0300

    docstring and main.yml fix

--- backend/food/apps.py
+++ backend/food/apps.py
@@ -1,7 +1,11 @@
+"""backend/food/apps.py."""
+
 from django.apps import AppConfig
 
 
 class FoodConfig(AppConfig):
+    """Конфигурация приложения Food."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'food'
     verbose_name = 'Фудграм'
